bf62f279c324761f123a57a49db5fe3bdd6a93a0
Sheikyon <sheikyon@proton.me>
2026-01-31 19:57:44 +0100

    Update strided_patch.h

--- native/kernels/strided_patch.h
+++ native/kernels/strided_patch.h
@@ -1,6 +1,6 @@
 #ifndef STRIDED_PATCH_H
 #define STRIDED_PATCH_H
 
-extern "C" void launch_dummy(float* d_ptr, int n);
+extern "C" void launch_placeholder(float* d_ptr, int n, void* stream_ptr);
 
 #endif
