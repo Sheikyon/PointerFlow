ff5d0be449b5f67d4b9fbd79bb365133e277b553
Sheikyon <sheikyon@proton.me>
2026-01-31 19:54:44 +0100

    Update pointerflow.h

--- native/pointerflow.h
+++ native/pointerflow.h
@@ -22,12 +22,19 @@ public:
     uintptr_t device_ptr() const;
     size_t byte_size() const { return size_; }
 
+    cudaStream_t stream() const { return stream_; }
+
+    void synchronize() const;
+
     void print_first_floats(int count) const;
+
+    void launch_dummy(int n_elements) const;
 
 private:
     void* host_ptr_ = nullptr;
     void* device_ptr_ = nullptr;
     size_t size_ = 0;
+    cudaStream_t stream_ = nullptr;
 };
 
 #endif
